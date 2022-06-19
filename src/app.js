7deaf19a02238e31a95b9ddc517cce79016dd063
Roman <roman200220022002@gmail.com>
2022-06-19 19:50:24 +0300

    mongoDbclient

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@ import express from 'express';
 import userRouter from '../routes/users.js';
 import { getClient, startConnection } from '../dbClient/mongodb.js';
 const app = express();
-const PORT = 3000;
-const connectionString = 'mongodb://localhost:27017';
+const PORT = 3000;;
 
 app.listen(PORT, err => {
     if (err) {
@@ -15,5 +14,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(userRouter);
-const client = getClient(connectionString);
+const client = getClient();
 startConnection(client);

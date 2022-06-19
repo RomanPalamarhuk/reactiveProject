54ec012d3d8c5f911b0cc7fa69a804874a483683
Roman <roman200220022002@gmail.com>
2022-06-19 19:50:15 +0300

    mongoDbClient

--- dbClient/mongodb.js
+++ dbClient/mongodb.js
@@ -1,13 +1,30 @@
 import { MongoClient } from 'mongodb';
 
-export function getClient(connectionString){
-    return new MongoClient(connectionString);
+const connenctionString = 'mongodb://localhost:27017/testdb';
+const collectionsNames = ['dataStream', 'users'];
+export function getClient(){
+    return new MongoClient(connenctionString);
 }
 export async function startConnection(client) {
     try {
         await client.connect();
-        console.log("Connection good")
+        console.log("Connection good");
+        await createCollections(collectionsNames);
     } catch (e) {
         console.error(e);
+    }
+}
+
+async function createCollections(namesList){
+    try{
+        const db = getClient().db();
+        let promiseArray = [];
+        collectionsNames.forEach(e => {
+            promiseArray.push(db.createCollection(e));
+        });
+        await Promise.allSettled(promiseArray);
+    }
+    catch (e){
+        console.log("Error: happened" + e);
     }
 }

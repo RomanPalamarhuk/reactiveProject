dc29afefaa472ddabdb93746b1497c5949f5b1c0
Roman <roman200220022002@gmail.com>
2022-06-20 14:46:13 +0300

    saveParsedStreamToDb

--- controllers/stream-reader/wikimediaListener.js
+++ controllers/stream-reader/wikimediaListener.js
@@ -1,12 +1,16 @@
-import  EventSource  from 'eventsource';
+import EventSource from 'eventsource';
+import { Timestamp } from 'mongodb';
+import { getClient } from '../../dbClient/mongodb.js';
 
 const URL = 'https://stream.wikimedia.org/v2/stream/recentchange';
-const TIME_STAMPT = 30000;
+const TIME_STAMPT = 10000;
 const eventSourse = new EventSource(URL);
 
 
 export function wikipediaListener() {
-    let data_to_save = [];
+    let promise_to_Save = [];
+    let recentTime = new Date().getTime();
+    const stream_file = getClient().db().collection('dataStream');
     eventSourse.onopen = () => {
         console.info('Opened Connection');
     }
@@ -14,12 +18,25 @@ export function wikipediaListener() {
     eventSourse.onerror = (event) => {
         console.log('Error', event);
     }
-
     eventSourse.onmessage = (event) => {
         const data = JSON.parse(event.data);
-        console.log(data);
-        // if (data.server_name === 'en.wikipedia.org') {
-        //     console.log(data);
-        // }
+        console.log({user: data.user, type: data.type, title: data.title, time: new Date(data.meta.dt)});
+        promise_to_Save.push(stream_file.insertOne({user: data.user, type: data.type, title: data.title, time: new Date(data.meta.dt)}));
+        if (new Date().getTime() - recentTime > TIME_STAMPT) {
+            recentTime = new Date().getTime();
+            let buffer = promise_to_Save;
+            promise_to_Save = [];
+            SaveToDb(buffer);
+
+        }
+    }
+}
+
+async function SaveToDb(buffer) {
+    try {
+        await Promise.allSettled(buffer);
+    }
+    catch (e) {
+        console.log("Error" + e);
     }
 }
